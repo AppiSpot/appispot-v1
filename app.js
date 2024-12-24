f08bd96b3a4f6743fc0e8aed7b7a94f434a90739
AppiSpot <ugacomp.server@gmail.com>
2024-12-24 23:01:38 +0800

    add the prometheus

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const client = require('prom-client');
 
 var app = express();
+const register = new client.Registry();
+
+// Default metrics collection
+client.collectDefaultMetrics({ register });
+
+// Custom example metric
+const httpRequestDuration = new client.Histogram({
+    name: 'http_request_duration_seconds',
+    help: 'HTTP request duration in seconds',
+    labelNames: ['method', 'route', 'status_code'],
+});
+register.registerMetric(httpRequestDuration);
+
+// Metrics endpoint
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+});
+
 module.exports = app;
 
 // setup config
